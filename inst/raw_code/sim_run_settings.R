702f673cc366eb1e48d13e59c2d0ac70d0deb850
Marco Simnacher <marco.simnacher@gmail.com>
2024-05-14 16:00:44 +0200

    settings include also squarred y

--- inst/raw_code/sim_run_settings.R
+++ inst/raw_code/sim_run_settings.R
@@ -4,15 +4,15 @@ setwd('inst')
 #### args =['/CI/ or /No_CI/', post_non_lin, eps_sigmaX, eps_sigmaY, eps_sigmaZ, embedding_orig, embedding_obs, confounder, response, CIT, CIT_params]
 #c("/CI/", "1", "0", "1", "0", "fastsurfer", "fastsurfer", "AS", "simulated", "RCOT", "1")
 settings <- data.frame(
-  dependence = rep(c('/CI/', '/No_CI/'), each = 24),
-  fct_relation = rep(rep(c('1','4','5'), each=4), 4),
-  eps_sigmaX = rep(rep(c(0, 3,0, 0), 6), 2),
-  eps_sigmaY = rep(1, 48),
-  eps_sigmaZ = rep(0, 48),
-  embedding_orig = rep('fastsurfer', 48),
-  embedding_obs = rep(c('fastsurfer', 'noisy', 'freesurfer', 'condVAE'), 12),
-  confounder = rep(rep(c("AS", "genes10"), each=12),2),
-  response = rep('simulated', 48)
+  dependence = rep(c('/CI/', '/No_CI/'), each = 32),
+  fct_relation = rep(rep(c('1','2','4','5'), each=4), 4),
+  eps_sigmaX = rep(rep(c(0, 3,0, 0), 8), 2),
+  eps_sigmaY = rep(1, 64),
+  eps_sigmaZ = rep(0, 64),
+  embedding_orig = rep('fastsurfer', 64),
+  embedding_obs = rep(c('fastsurfer', 'noisy', 'freesurfer', 'condVAE'), 16),
+  confounder = rep(rep(c("AS", "genes10"), each=16),2),
+  response = rep('simulated', 64)
 )
 
 cits <- c('RCOT 1', 'WALD', 'kpc_graph 2 10', 'CMIknn', 'FCIT')
